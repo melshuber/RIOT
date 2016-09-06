a9bf32cc9707a8dd14ed7cf149f4e92e456b9806
Martin Elshuber <martin.elshuber@theobroma-systems.com>
2016-09-06 13:03:34 +0200

    updated comments

--- core/rmutex.c
+++ core/rmutex.c
@@ -73,23 +73,23 @@ void rmutex_lock(rmutex_t *rmutex)
          * Note for Case 1:
          *
          *     Because the mutex rmutex->owner is only written be the
-         *     owner (us), rmutex->owner stays constant througout the
+         *     owner (me), rmutex->owner stays constant througout the
          *     complete call and rmutex->refcount is protected
-         *     (reas/write) by the mutex.
+         *     (read/write) by the mutex.
          */
 
-        /* ensure that owner is atomically, since we need a consistent value */
+        /* ensure that owner is read atomically, since I need a consistent value */
         owner = atomic_load_explicit( &rmutex->owner, memory_order_relaxed);
         DEBUG("rmutex %" PRIi16" : mutex held by %" PRIi16" \n", thread_getpid(), owner);
 
-        /* Case 2 : Another Thread hold the mutex */
+        /* Case 1: Mutex is not held by me */
         if ( owner != thread_getpid() ) {
             /* wait for the mutex */
             DEBUG("rmutex %" PRIi16" : locking mutex\n", thread_getpid());
 
             mutex_lock(&rmutex->mutex);
         }
-        /* Case 1 : We already hold the mutex */
+        /* Case 2: Mutex is held be me (relock) */
         else {
             assert(rmutex->refcount>0);
         }
@@ -97,11 +97,10 @@ void rmutex_lock(rmutex_t *rmutex)
 
     DEBUG("rmutex %" PRIi16" : I am now holding the mutex\n", thread_getpid());
 
-    /* We are holding the recursive mutex */
-
+    /* I am holding the recursive mutex */
     DEBUG("rmutex %" PRIi16" : settting the owner\n", thread_getpid());
 
-    /* ensure that owner is written only once */
+    /* ensure that owner is written atomically, since others need a consistent value */
     atomic_store_explicit(&rmutex->owner, thread_getpid(), memory_order_relaxed);
 
     DEBUG("rmutex %" PRIi16" : increasing refs\n", thread_getpid());
@@ -116,23 +115,23 @@ int rmutex_trylock(rmutex_t *rmutex)
 
     /* try to lock the mutex */
     if (mutex_trylock(&rmutex->mutex) == 0) {
-        /* ensure that owner is read only once */
+        /* ensure that owner is read atomically, since I need a consistent value */
         owner = atomic_load_explicit( &rmutex->owner, memory_order_relaxed);
 
-        /* Case 2 : Another Thread hold the mutex */
+        /* Case 1: Mutex is not held by me */
         if ( owner != thread_getpid() ) {
             /* wait for the mutex */
             return 0;
         }
-        /* Case 1 : We already hold the mutex */
+        /* Case 2: Mutex is held be me (relock) */
         else {
             assert(rmutex->refcount>0);
         }
     }
 
-    /* we are holding the recursive mutex */
+    /* I am are holding the recursive mutex */
 
-    /* ensure that owner is written only once */
+    /* ensure that owner is written atomically, since others need a consistent value */
     atomic_store_explicit(&rmutex->owner, thread_getpid(), memory_order_relaxed);
 
     /* increase the refcount */
@@ -150,7 +149,7 @@ void rmutex_unlock(rmutex_t *rmutex)
     /* decrement refcount */
     rmutex->refcount--;
 
-    /* check if we still hold the mutex */
+    /* check if I should still hold the mutex */
     if (rmutex->refcount == 0) {
         /* if not release the mutex */
 
